36b70e77b53e305ff0b983de7f00bfb78748ad16
Halocau <buitienquat2k3@gmail.com>
2025-05-11 18:02:03 +0700

    30 JSX overview

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,23 @@
 //() => {}
+//JSX chỉ có 1 cha duy nhấtast
 // component = html + css + js
+//fragment: giúp ko phá vỡ cấu trúc html (ko render bừa bãi html)
+// phải dùng className thay vì class vì dùng class sẽ bị trùng với js
+// trong jsx thì css inline là 1 object
+
+import './style.css';
+
 const MyCompomnent = () => { //tag
     return (
-        <div>
-            Bùi Tiến Quát
-        </div>
+        <>
+            <div style={
+                {
+                    color: 'blue',
+                    backgroundColor: 'red',
+                }
+            }>My Components</div>
+            <div className="test">fake</div>
+        </>
     );
 }
 export default MyCompomnent;
