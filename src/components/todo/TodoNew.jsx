7d2e8ebf6ad7a5717e7248c596faeb0631e380e3
Halocau <buitienquat2k3@gmail.com>
2025-05-12 21:31:02 +0700

    36 dom event basic

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,11 +2,31 @@ const TodoNew = (props) => {
     console.log("Check props TodoNew", props.addNewTodo);
 
     const { addNewTodo } = props;
-    addNewTodo("test");
+    // addNewTodo("test");
+    const handlerClick = () => {
+        alert("Click me");
+    }
+
+    // const handleOnChange = (event) => {
+    //     console.log(">>> onChange", event.target.value);
+    // }
+
+    const handleOnChange = (name) => {
+        console.log(">>> onChange", name);
+    }
+
     return (
         <div className="todo-input">
-            <input className='search' type="text" placeholder="Enter your task" />
-            <button className='todo-button-add'>Add</button>
+            <input className='search'
+                type="text"
+                placeholder="Enter your task"
+                // onChange={handleOnChange}
+                onChange={(event) => { handleOnChange(event.target.value) }}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handlerClick}// tham chiếu hàm
+            >Add</button>
         </div>
     );
 }
