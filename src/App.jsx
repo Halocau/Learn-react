6d2148479be8f2cddeaf22507f12e96f1c1e1903
Halocau <buitienquat2k3@gmail.com>
2025-05-12 14:30:27 +0700

    34 learn props

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import TodoNew from './components/todo/TodoNew'
 import TodoData from './components/todo/TodoData'
 import reactLogo from './assets/react.svg'
 const App = () => {
+  const quat = "Quát hihi";
+  const a = 22;
+  const data = {
+    address: "HaNoi",
+    country: "VietNam"
+  }
+  //{Key:value}
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
 
       <TodoNew />
 
-      <TodoData />
+      <TodoData
+        name={quat}
+        age={a}
+        data={data}
+      />
 
       <div className='todo-image'>
         <img src={reactLogo} className="logo react" alt="React logo" />
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,8 +1,26 @@
-const TodoData = () => {
+// const TodoData = ({ name, age, data }) => {
+const TodoData = (props) => {
+    //props: là 1 biến object {}
+    // {
+    //     name: "top 1 server",
+    //     age: 22,
+    //     data: { }
+    // }
+
+    //BEGINER
+    // const name = props.name;
+    // const age = props.age;
+    // const data = props.data;
+
+    //JUNIOR
+    const { name, age, data } = props; // cần dùng thuộc tính nào thì lấy thuộc tính đó
+
+    console.log("Check data props => ", props);
     return (
         <div className='todo-data'>
-            <div>Learn</div>
-            <div>Learn 1</div>
+            <div>My name is {name}</div>
+            <div>Learn {age}</div>
+            <div>Learn 1 {data.address}</div>
         </div>
     );
 }
