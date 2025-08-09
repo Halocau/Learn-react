e85938f689ef1fecf1e1b32f8eb163e174966c68
Halocau <buitienquat2k3@gmail.com>
2025-08-09 15:26:47 +0700

    72. useEffect hook

--- src/components/user/userTable.jsx
+++ src/components/user/userTable.jsx
@@ -1,28 +1,22 @@
 import { Space, Table, Tag } from 'antd';
 import { fetchAllUserAPI } from '../../services/ApiService';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const UserTable = () => {
-    const loadUser = async () => {
-        console.log("run load user START")
-        const res = await fetchAllUserAPI();
-        setDataUsers(res.data)
-        console.log("run load user END", res)
-    }
-    loadUser();
+
     const [dataUsers, setDataUsers] = useState([
-        {
-            name: 'Bui Tien Quat',
-            age: '22',
-            address: 'Dai hoc FPT'
-        },
-        {
-            name: 'Bui Tien',
-            age: '23',
-            address: 'Ha Noi'
-        }
+        // {
+        //     name: 'Bui Tien Quat',
+        //     age: '22',
+        //     address: 'Dai hoc FPT'
+        // }
     ]);
 
+    //empty array => run once
+    useEffect(() => {
+        console.log(">>> Run effect 111");
+        loadUser();
+    }, []);//function
 
     const columns = [
         {
@@ -39,33 +33,19 @@ const UserTable = () => {
         }
     ];
 
-    // const data = [
+    const loadUser = async () => {
+        const res = await fetchAllUserAPI();
+        setDataUsers(res.data)
+    }
 
-    //     {
-    //         key: '1',
-    //         name: 'John Brown',
-    //         age: 32,
-    //         address: 'New York No. 1 Lake Park',
-    //         tags: ['nice', 'developer'],
-    //     },
-    //     {
-    //         key: '2',
-    //         name: 'Jim Green',
-    //         age: 42,
-    //         address: 'London No. 1 Lake Park',
-    //         tags: ['loser'],
-    //     },
-    //     {
-    //         key: '3',
-    //         name: 'Joe Black',
-    //         age: 32,
-    //         address: 'Sydney No. 1 Lake Park',
-    //         tags: ['cool', 'teacher'],
-    //     },
-    // ];
+    console.log(">>> run render")
 
     return (
-        <Table columns={columns} dataSource={dataUsers} />
+        <Table
+            columns={columns}
+            dataSource={dataUsers}
+            rowKey={"_id"}
+        />
     );
 }
 
